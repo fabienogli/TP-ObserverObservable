a09d9d149ee920600c18283be571312e1f0d0a38
Fabien <fabien.ogli@etu.univ-lyon1.fr>
2018-09-21 15:07:01 -0400

    xml configuration + refact of QuizzMasterService + IoC by setter

--- quizz/src/devoir2/question1/QuizProgram.java
+++ quizz/src/devoir2/question1/QuizProgram.java
@@ -1,17 +1,20 @@
 package devoir2.question1;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class QuizProgram {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-        ApplicationContext context = new AnnotationConfigApplicationContext(QuizzServiceConstructor.class);
-		QuizzMasterService quizMasterService = context.getBean(QuizzMasterService.class);
-	}
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.setProperty("spring.profiles.active", "constructor");
+
+//        ApplicationContext quizz = new ClassPathXmlApplicationContext("devoir2/question1/QuizzBeans.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("devoir2/question1/BeanFactory.xml");
+        QuizzMasterService quizzMasterService = (QuizzMasterService) context.getBean("quizzConstructor");
+    }
 
 }
--- quizz/src/devoir2/question1/QuizzMasterService.java
+++ quizz/src/devoir2/question1/QuizzMasterService.java
@@ -1,65 +1,92 @@
 package devoir2.question1;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-@Component
 public class QuizzMasterService {
 	private JFrame ecran;
 	private JRadioButton [] lesBoutons;
 	private QuizzMaster quizMaster;
 
-	@Autowired
-	public QuizzMasterService(QuizzMaster quizzMaster)
+	public QuizzMasterService()
 	{
-	    this.quizMaster = quizzMaster;
 		this.ecran = new JFrame();
-		lesBoutons = new JRadioButton [4];
-		ecran.setSize( 450, 200 );
-		this.ecran.setTitle(quizMaster.getType());
+        lesBoutons = new JRadioButton [4];
+        this.ecran.setSize( 450, 200 );
 		// Fermeture de la fen�tre
-		ecran.addWindowListener( new WindowAdapter() {
+		this.ecran.addWindowListener( new WindowAdapter() {
 		public void windowClosing( WindowEvent we ) {
 			QuizzMasterService.this.terminer();
 		}
 		} );
-		// Le panneau central
-		JPanel panneauCentral = new JPanel( new BorderLayout() );
-		ecran.add( panneauCentral );
-		ActionListener listener = new ActionListener() {
-	         public void actionPerformed (ActionEvent e) {
-	        	 AbstractButton aButton = (AbstractButton) e.getSource();
-	        	 String message=aButton.getText();
-	        	 if(message.equals(quizMaster.getChoix(quizMaster.getReponse())))
-	        		 JOptionPane.showMessageDialog( ecran, "bonne r�ponse" );
-	        	 else
-	        		 JOptionPane.showMessageDialog( ecran, "mauvaise r�ponse" );         
-	        	 
-	         };
-	      };
-		JPanel panneauQuestion= new JPanel( new FlowLayout( FlowLayout.LEADING ) );
-		panneauQuestion.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder()));
-		JLabel monlabel=new JLabel(quizMaster.getQuestion());
-		panneauQuestion.add(monlabel);
-		panneauCentral.add( panneauQuestion, BorderLayout.NORTH );
-		JPanel panneauChoix = new JPanel( new FlowLayout( FlowLayout.LEFT ) );
-		ButtonGroup groupe = new ButtonGroup ();
-		for(int i=0; i<4; i++)
-		{
-			lesBoutons[i] = new JRadioButton (quizMaster.getChoix(i));
-			groupe.add(lesBoutons[i]);
-			panneauChoix.add(lesBoutons[i]);
-			lesBoutons[i].addActionListener(listener);
-		}
-		panneauCentral.add( panneauChoix, BorderLayout.CENTER);
-		ecran.setVisible( true );
 	}
+
+    public QuizzMasterService(QuizzMaster quizzMaster) {
+        this();
+	    this.setQuizMaster(quizzMaster);
+    }
+
+    private void setPanneaux() {
+        JPanel panneauCentral = new JPanel( new BorderLayout() );
+        JPanel panneauChoix = new JPanel( new FlowLayout( FlowLayout.LEFT ) );
+        // Le panneau central
+        ecran.add( panneauCentral );
+        JPanel panneauQuestion = getPanneauQuestion();
+        listenerButton(getActionListener(), panneauChoix);
+        panneauCentral.add( panneauChoix, BorderLayout.CENTER);
+        panneauCentral.add( panneauQuestion, BorderLayout.NORTH);
+    }
+
+    private JPanel getPanneauQuestion() {
+        JPanel panneauQuestion= new JPanel( new FlowLayout( FlowLayout.LEADING ) );
+        panneauQuestion.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder()));
+        JLabel monlabel = config();
+        panneauQuestion.add(monlabel);
+        return panneauQuestion;
+    }
+
+    private void listenerButton(ActionListener listener, JPanel panneauChoix) {
+        ButtonGroup groupe = new ButtonGroup ();
+        for(int i=0; i<4; i++)
+        {
+            lesBoutons[i] = new JRadioButton (quizMaster.getChoix(i));
+            groupe.add(lesBoutons[i]);
+            panneauChoix.add(lesBoutons[i]);
+            lesBoutons[i].addActionListener(listener);
+        }
+    }
+
+    private ActionListener getActionListener() {
+        return new ActionListener() {
+                 public void actionPerformed (ActionEvent e) {
+                     AbstractButton aButton = (AbstractButton) e.getSource();
+                     String message=aButton.getText();
+                     if(message.equals(quizMaster.getChoix(quizMaster.getReponse())))
+                         JOptionPane.showMessageDialog( ecran, "bonne r�ponse" );
+                     else
+                         JOptionPane.showMessageDialog( ecran, "mauvaise r�ponse" );
+
+                 };
+              };
+    }
+
+    private JLabel config() {
+        //return label
+        JLabel monlabel=new JLabel(quizMaster.getQuestion());
+        return monlabel;
+    }
 	private void terminer() {
-		
 		this.ecran.dispose();			
 		System.exit( 0 );
 	}
+
+    public void setQuizMaster(QuizzMaster quizzMaster) {
+	    this.quizMaster = quizzMaster;
+        setPanneaux();
+        //setTitle
+        this.ecran.setTitle(quizMaster.getType());
+        this.ecran.setVisible( true );
+
+    }
 }
